df8635dae62a7e9cd5085e2279c181cac22a0e1a
bernizhel <detrop.1995@gmail.com>
2023-08-21 18:31:09 +0300

    feat: bubble-sort

--- problems/bubble-sort/bubble-sort.js
+++ problems/bubble-sort/bubble-sort.js
@@ -1,7 +1,15 @@
 // https://sortvisualizer.com/bubblesort/
 // https://en.wikipedia.org/wiki/Bubble_sort
 
+// time: O(n^2)
+// space: O(1)
 export function bubbleSort(arr) {
-
-    return arr;
+  for (let j = 0; j < arr.length; j++) {
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i] < arr[i - 1]) {
+        [arr[i], arr[i - 1]] = [arr[i - 1], arr[i]];
+      }
+    }
+  }
+  return arr;
 }
