787e7b8ad3d385e889b03b4757ed34d3fd06ef91
bernizhel <detrop.1995@gmail.com>
2023-08-21 20:40:42 +0300

    feat: binary-search

--- problems/binary-search/binary-search.js
+++ problems/binary-search/binary-search.js
@@ -1,5 +1,18 @@
 // https://en.wikipedia.org/wiki/Binary_search_algorithm
 
 export function binarySearch(arr, value) {
-
+    let min = 0;
+    let max = arr.length - 1;
+    let middle = Math.round((min + max) / 2);
+    while (min <= max) {
+        if (value < arr[middle]) {
+            max = middle - 1;
+        } else if (value > arr[middle]) {
+            min = middle + 1;
+        } else {
+            return middle;
+        }
+        middle = Math.round((min + max) / 2);
+    }
+    return undefined;
 }
