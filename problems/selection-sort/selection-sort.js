e61652672c343050e1a1c63c0f4f4fc90eec7ae0
bernizhel <detrop.1995@gmail.com>
2023-08-21 18:45:57 +0300

    feat: selection-sort

--- problems/selection-sort/selection-sort.js
+++ problems/selection-sort/selection-sort.js
@@ -2,6 +2,14 @@
 // https://en.wikipedia.org/wiki/Selection_sort
 
 export function selectionSort(arr) {
-
-    return arr;
+  for (let i = 0; i < arr.length; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+    [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+  }
+  return arr;
 }
