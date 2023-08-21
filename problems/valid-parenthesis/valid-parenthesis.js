1acacf39a00d5fec3e8472b8b25f66f93f42db34
bernizhel <detrop.1995@gmail.com>
2023-08-21 19:53:47 +0300

    feat: valid-parenthesis

--- problems/valid-parenthesis/valid-parenthesis.js
+++ problems/valid-parenthesis/valid-parenthesis.js
@@ -1,5 +1,21 @@
 // https://leetcode.com/problems/valid-parentheses/
 
 export function isValid(str) {
-
+    const map = {
+        "(": ")",
+        "[": "]",
+        "{": "}",
+    };
+    const stack = [];
+    for (const char of str) {
+        if (["(", "[", "{"].includes(char)) {
+            stack.push(char);
+            continue;
+        }
+        const current = stack.pop();
+        if (map[current] !== char) {
+            return false;
+        }
+    }
+    return true;
 }
