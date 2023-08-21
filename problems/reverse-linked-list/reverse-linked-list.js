cfd8ae1698b8152085a4b73f973ed876413e8f4f
bernizhel <detrop.1995@gmail.com>
2023-08-21 21:02:17 +0300

    feat: reverse-linked-list

--- problems/reverse-linked-list/reverse-linked-list.js
+++ problems/reverse-linked-list/reverse-linked-list.js
@@ -8,6 +8,33 @@
  * }
  */
 
-export function reverseList(head) {
+const list = {
+    val: 1,
+    next: {
+        val: 2,
+        next: {
+            val: 3,
+            next: {
+                val: 4,
+                next: {
+                    val: 5,
+                    next: null,
+                },
+            },
+        },
+    },
+};
 
+export function reverseList(head) {
+    let temp = head.next;
+    let curr = head.next;
+    head.next = null;
+    while (curr.next !== null) {
+        temp = curr.next;
+        curr.next = head;
+        head = curr;
+        curr = temp;
+    }
+    curr.next = head;
+    return temp;
 }
